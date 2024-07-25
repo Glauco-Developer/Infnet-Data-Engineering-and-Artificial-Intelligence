294cd737bb6fe5e97c94f792d39eb106afbdc4db
glaucorusso <glaucorusso@ebow.ie>
2024-07-25 16:33:09 -0300

    added interatividade-em-paginas-web

---
+++ interatividade-em-paginas-web/js/app.js
@@ -0,0 +1,17 @@
+// let cadastroPessoa = {
+//     nome: ["Glauco", "Russo"],
+//     idade: 36,
+//     email: "glauco.developer@gmail.com",
+//     saudacao: function() {
+//         alert("Oi, meu nome é " + this.nome[0]);
+    // }
+// }
+// document.write(cadastroPessoa.nome[0]+"<br>");
+// document.write(cadastroPessoa.nome[1]+"<br>");
+// cadastroPessoa.saudacao();
+
+let user1 = new Person('Jo', 'Russo', 36, 'verde');
+console.log(user1.firstName);
+
+let user2 = new Person('Glauco', 'Russo', 36, 'verde');
+console.log(user2.firstName);
---
+++ interatividade-em-paginas-web/js/objects.js
@@ -0,0 +1,8 @@
+class Person {
+    constructor(first, last, age, eye) {
+        this.firstName = first;
+        this.lastName = last;
+        this.age = age;
+        this.eyeColor = eye;
+    }
+}
